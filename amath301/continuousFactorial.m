a7ec36d362a57412751a8547b77f3f22358dbdf8
justinthompson593 <justinthompson593@gmail.com>
2014-11-09 15:38:01 -0800

    commit

--- amath301/continuousFactorial.m
+++ amath301/continuousFactorial.m
@@ -3,7 +3,7 @@ function [ Y ] = continuousFactorial( X )
 % Y = Gamma(X+1) = X! 
 %              (for X = a + i*b with a > 0) 
 
-Y = integral(@(t) t.^(x).*exp(-t),0,Inf);
+Y = integral(@(t) t.^(X).*exp(-t),0,Inf);
 
 end
 
