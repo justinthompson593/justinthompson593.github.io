ef43aad65625c285a197550112e30e09ebe87774
Justin Thompson <justinthompson593@users.noreply.github.com>
2017-08-29 19:20:21 -0700

    Create PasswordSafe.cpp

---
+++ SoftwareDrop/PasswordSafe.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string.h>
+#include <fstream>
+/***********************************************************************
+*
+* momsPasswords.cpp
+*
+*	USAGE: 	$ momsPasswords [args]
+*
+*	DESCR: 	
+*
+*	OUTPUT:	
+***********************************************************************/
+
+using namespace std;
+
+
+
+int main(int argc, char* argv[]){
+	
+	
+	return 0;
+}
+
+
+
